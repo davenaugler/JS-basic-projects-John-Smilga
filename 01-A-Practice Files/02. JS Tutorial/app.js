f0d161fd6ec2ae3462426ad270892b977ba72708
Dave Naugler <davenaugler@gmail.com>
2024-08-21 21:15:26 -0600

    JS Variables

---
+++ 01-A-Practice Files/02. JS Tutorial/app.js
@@ -0,0 +1,11 @@
+// document.write('hello world');
+// alert('hello world');
+// console.log('hello world');
+
+// document.write('hello world');
+// alert('hello world');
+// console.log('hello world');
+
+document.write({name: 'dave'});
+alert({name: 'dave'});
+console.log({name: 'dave'});
---
+++ 01-A-Practice Files/03. Variables/app.js
@@ -0,0 +1,23 @@
+// Variable - Most Basic Building Block
+// Variables - Store, Access, Modify === Value
+// Declare, Assignment Operator, Assign Value
+
+
+
+
+
+
+
+let name = "Dave shrimp taco VI";
+console.log(name);
+// some code here
+console.log(name);
+// some code here
+console.log(name);
+// some code here
+console.log(name);
+// some code here
+console.log(name);
+// some code here
+console.log(name);
+// some code here
