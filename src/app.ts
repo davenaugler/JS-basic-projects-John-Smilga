cbdce5ac61d4d454276193438c4ec86c23d8111d
Dave Naugler <davenaugler@gmail.com>
2024-08-22 21:38:16 -0600

    Deleted most content & switched to Vite + TypeScrip

---
+++ src/app.ts
@@ -0,0 +1,17 @@
+const number:number = 34;
+const number2:number = 2.456;
+const number3:string = '2.456';
+const add:number = number + number2;
+
+const message:string = "Hello Dave. I'm working.";
+
+console.log(add);
+console.log(number);
+console.log(number3);
+console.log(message);
+
+const website:string = "youtube";
+const url:string = "https://www." + website + ".com";
+console.log(url);
+
+
